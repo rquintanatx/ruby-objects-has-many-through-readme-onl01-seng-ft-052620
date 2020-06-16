b5f6cd06efb55dd364792ef87d93fe8dacbce27a
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 06:53:06 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -7,7 +7,14 @@ class Waiter
     @name = name
     @yrs_experience = yrs_experience
     @@all << self
+  end
+  
+  def meals
+    Meals.all.select {|meal| meal.waiter == self}
   end 
+  
+  def best_tipper
+    meals.
   
   def new_meal(customer, total, tip = 0)
     Meal.new(self, customer, total, tip)
