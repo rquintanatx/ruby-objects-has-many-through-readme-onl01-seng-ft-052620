dd9ac2827866255bacab0c6090f33dbc9a153cff
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 06:23:13 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
+  attr_accessor :name,  :age
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self
+  end 
+  
+  def meals
+    Meal.all select {|meal| meal.customer}
+  end 
+  
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end 
+  
+  def self.all
+    @@all 
+  end
 end
